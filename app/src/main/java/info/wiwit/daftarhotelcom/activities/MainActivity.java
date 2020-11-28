9d548f8db74e4163ba3b8989807a4686fcdc9a68
Wiwit Widhya <wiwitwidhya@gmail.com>
2020-11-28 15:36:25 +0700

    menambahkan commit file

---
+++ app/src/main/java/info/wiwit/daftarhotelcom/activities/MainActivity.java
@@ -0,0 +1,105 @@
+package info.wiwit.daftarhotelcom.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import info.wiwit.daftarhotelcom.R;
+import info.wiwit.daftarhotelcom.adapter.MainAdapter;
+import info.wiwit.daftarhotelcom.decoration.LayoutMarginDecoration;
+import info.wiwit.daftarhotelcom.model.ModelMain;
+import info.wiwit.daftarhotelcom.utils.Tools;
+
+public class MainActivity extends AppCompatActivity implements MainAdapter.onSelectData {
+
+    RecyclerView rvMainMenu;
+    LayoutMarginDecoration gridMargin;
+    ModelMain mdlMainMenu;
+    List<ModelMain> lsMainMenu = new ArrayList<>();
+    TextView tvToday;
+    String hariIni;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility
+                    (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        tvToday = findViewById(R.id.tvDate);
+
+        GridLayoutManager mLayoutManager = new GridLayoutManager(this, 2,
+                RecyclerView.VERTICAL, false);
+
+        gridMargin = new LayoutMarginDecoration(2, Tools.dp2px(this, 4));
+
+
+        //get Time Now
+        Date dateNow = Calendar.getInstance().getTime();
+        hariIni = (String) DateFormat.format("EEEE", dateNow);
+        getToday();
+        setMenu();
+    }
+
+    private void getToday() {
+        Date date = Calendar.getInstance().getTime();
+        String tanggal = (String) DateFormat.format("d MMMM yyyy", date);
+        String formatFix = hariIni + ", " + tanggal;
+        tvToday.setText(formatFix);
+    }
+
+    private void setMenu() {
+        mdlMainMenu = new ModelMain("Hotel", R.drawable.ic_hotel);
+        lsMainMenu.add(mdlMainMenu);
+
+        MainAdapter myAdapter = new MainAdapter(lsMainMenu, this);
+
+
+    }
+
+    @Override
+    public void onSelected(ModelMain mdlMain) {
+        switch (mdlMain.getTxtName()) {
+            case "Hotel":
+                startActivityForResult(new Intent(MainActivity.this, HotelActivity.class), 1);
+                break;
+        }
+    }
+
+    //set Transparent Status bar
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+}
---
+++ app/src/main/java/info/wiwit/daftarhotelcom/api/Api.java
@@ -0,0 +1,7 @@
+package info.wiwit.daftarhotelcom.api;
+
+public class Api {
+
+    public static String Hotel = "https://dev.farizdotid.com/api/purwakarta/hotel";
+
+}
